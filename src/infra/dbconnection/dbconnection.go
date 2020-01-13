bcf6fd9b1416e1a7df1e31da94e7864cce1aef7c
Gabriel <gabriel@dosanjos.net.br>
2020-01-12 21:10:28 -0300

    Atualização

--- src/infra/dbconnection/dbconnection.go
+++ src/infra/dbconnection/dbconnection.go
@@ -25,12 +25,11 @@ func init() {
 	username := os.Getenv("db_user")
 	password := os.Getenv("db_pass")
 	dbName := os.Getenv("db_name")
-	//dbHost := os.Getenv("db_host")
+	dbHost := os.Getenv("db_host")
 	//dbPort := os.Getenv("db_port")
 	//dbtype := os.Getenv("db_type")
 
-	//dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
-	dbURI := fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s", username, dbName, password) //Build connection string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
 
 	conn, err := gorm.Open("postgres", dbURI)
 	if err != nil {
